48c8f83bd21c571bff3c25c3092018d50758a00b
Yuliya Horobets <futurism.flying.ufo@gmail.com>
2020-09-08 14:56:30 +0300

    second commit beta

--- javascript/climat.js
+++ javascript/climat.js
@@ -1,21 +1,21 @@
-function calc(){
+function calc() {
   var a = parseInt(document.querySelector("#value1").value);
   var op = document.querySelector("#operator").value;
   var calculate;
 
 
- if (op == "div") {
-  calculate = Math.round(a/1.5);
-} else if (op == "mul") {
-  calculate = Math.round(a*1.5);
-}
-//var n = /^[0-9]+$/;
-if (isNaN(a)){ //&& a.value.match(n)
-  document.querySelector("#result").innerHTML = "Please, enter a number.";
-} else if (calculate < 0) {
-  document.querySelector("#result").innerHTML = "Sorry, negative age is not allowed.";
-} else {
-  document.querySelector("#result").innerHTML = calculate;
-}
+  if (op == "div") {
+    calculate = Math.round(a / 1.5);
+  } else if (op == "mul") {
+    calculate = Math.round(a * 1.5);
+  }
+  //var n = /^[0-9]+$/;
+  if (isNaN(a)) { //&& a.value.match(n)
+    document.querySelector("#result").innerHTML = "Please, enter a number.";
+  } else if (calculate < 0) {
+    document.querySelector("#result").innerHTML = "Sorry, negative age is not allowed.";
+  } else {
+    document.querySelector("#result").innerHTML = calculate;
+  }
 
 }
--- javascript/toggle.js
+++ javascript/toggle.js
@@ -1,15 +1,14 @@
 let toggleStatus = true;
 
-let makeToggle = function(){
+let makeToggle = function () {
   let getWrapper = document.querySelector(".toggle-wraper");
   let getToggleButton = document.querySelector(".toggle-button");
 
-  if (toggleStatus === true){
+  if (toggleStatus === true) {
     getWrapper.style.opacity = "1";
     getToggleButton.style.opacity = "1";
     toggleStatus = false;
-  }
-  else if (toggleStatus === false){
+  } else if (toggleStatus === false) {
     getWrapper.style.opacity = "0";
     getToggleButton.style.opacity = "1";
     toggleStatus = true;
